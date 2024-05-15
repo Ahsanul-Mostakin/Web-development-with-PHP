ec23bf8b743bd9124701e3afbb51a6ba02fc82f7
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-05-15 13:59:14 +0600

    multidimensional_array

---
+++ Working with data/multidimensional_array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $data = [
+        'mostakin' => ['Salad', 'Kacci'],
+        'roktim' => 'Burger',
+        'Urmi' => 'Pizza'
+    ];
+    var_dump($data['mostakin'][1]);
+    ?>
+</body>
+</html>
