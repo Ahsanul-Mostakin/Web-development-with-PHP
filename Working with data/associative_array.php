74424f7f021f282d98980bd442f1da194f6f7f5d
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-05-15 13:56:57 +0600

    associative_array

---
+++ Working with data/associative_array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $data = ['Mostakin' => 'Salad', 'Roktim' => 'Burger', 'Dipta' => 'Pizza'];
+
+    $data ['Nobo'] = 'Chicken';
+    $data['Sourov'] = 'Tomato soup';
+
+    var_dump($data);
+</body>
+</html>
