407ba92c7b865e9255633b8a14349a20ed891d5d
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-05-21 19:19:38 +0600

    update

---
+++ Adding logic/terminology.php
@@ -0,0 +1,15 @@
+ <?php
+ 
+ $score = 50;
+
+ if($scoe > 90){
+    var_dump('A');
+ }else if ($score > 70){
+     var_dump('B');
+ }else if ($score > 60){
+    var_dump('C');
+ }else {
+    var_dump('F');
+ }
+
+ ?>
