a90404db46e34ff839429f0b555b974d9cef312a
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-05-21 20:22:57 +0600

    foreach loop

---
+++ Adding logic/for each.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $names = ['Jhon', 'Jane', 'Mostakin'];
+
+     foreach($name as $key => $name){
+        var_dump($key);
+        var_dump($name);
+     }
+
+    ?>
+</body>
+</html>
