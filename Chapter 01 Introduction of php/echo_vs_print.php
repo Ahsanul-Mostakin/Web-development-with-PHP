eaa11df73a78478a3f0dc26b6f147d8eec1cca92
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-12-28 19:04:56 +0600

    Added Chapter 01: Introduction of PHP

---
+++ Chapter 01 Introduction of php/echo_vs_print.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP echo vs print method</title>
+</head>
+<body>
+    <h1>Ahsanul Mostakin</h1>
+    <?php
+    echo "<h1>Mostakin code</h1>","How are you Ahsanul Mostakin?";
+    echo "</br>";
+    print "Angel Sadia";
+    $a =print (37+3);
+    echo ($a);
+    ?>
+</body>
+</html>
---
+++ Chapter 01 Introduction of php/hello_world.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Hello World Program</title>
+</head>
+<body>
+    <H1>Ahsanul Mostakin</H1>
+    <?php
+    echo "<h3>Hello World</h3>";
+    ?>
+</body>
+</html>
