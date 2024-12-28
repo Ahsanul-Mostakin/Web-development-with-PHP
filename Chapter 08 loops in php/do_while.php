f1d798af5b38013ba54f984cb06078a50fd9ea92
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-12-28 19:04:57 +0600

    Added Chapter 08: Loops in PHP

---
+++ Chapter 08 loops in php/do_while.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $n=10;
+    do{
+        echo "PHP programing  <br/>";
+        $n --;
+    }while ($n>=1);
+
+    ?>
+</body>
+</html>
---
+++ Chapter 08 loops in php/for_loops.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($n=10;$n>=1; $n--){
+        echo "$n <br/>";
+    }
+    ?>
+</body>
+</html>
